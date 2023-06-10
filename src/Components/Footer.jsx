e3c0c053653e1a281506ba9f5e36b893108bf971
NICKY-TECH <tinadesk8@gmail.com>
2023-06-10 22:40:49 +0100

    button size fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -20,6 +20,17 @@ export function Footer() {
         </a>
         .
       </p>
+      <div>
+        <div
+          className="mt-8 text-center"
+          style={{ backgroundColor: "#D0DBF1" }}
+        >
+          Hertechtrail cohort 6 Capstone project
+          <p>
+            Designed by <span className="font-bold">Team 7</span>
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
